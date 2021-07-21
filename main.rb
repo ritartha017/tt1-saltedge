4dbb3f9380d8f940ee3283bff2ec160872b1a6a4
Mira <72543014+ritartha017@users.noreply.github.com>
2021-07-21 21:17:50 +0300

    Update main.rb

--- main.rb
+++ main.rb
@@ -16,21 +16,21 @@ def create_playlist_for_my_user
     @user = @user.merge!(Api::Client::get_profile_data(access_token))
 
     @new_playlist = Api::Client::create_playlist(access_token, @user['id'], 'My new playlist', 'My awesome description!')
-    tracks = [
-        'spotify:track:18bjR8nOi853iJS8XBpdHU',
-        'spotify:track:7hS3i9y9gi81OOFT1rCfSU',
-        'spotify:track:6LRn9mc7W1lsxyirdYegH4',
-        'spotify:track:11rsK9GDZ9UdWsxtDqnMmN',
-        'spotify:track:631B0Vtr5oMc6GJJNXZLnp',
-        'spotify:track:46Z1ogcP9C9UxBD1aO2iRR',
-        'spotify:track:2LJZE1yXy9SdglruufEDHG'
-    ]
+    
+    tracks = ['spotify:track:18bjR8nOi853iJS8XBpdHU',
+              'spotify:track:7hS3i9y9gi81OOFT1rCfSU',
+              'spotify:track:6LRn9mc7W1lsxyirdYegH4',
+              'spotify:track:11rsK9GDZ9UdWsxtDqnMmN',
+              'spotify:track:631B0Vtr5oMc6GJJNXZLnp',
+              'spotify:track:46Z1ogcP9C9UxBD1aO2iRR',
+              'spotify:track:2LJZE1yXy9SdglruufEDHG'
+             ]   
 
     playlist_id =  @new_playlist["uri"]
     Api::Client::add_tracks_to_playlist(access_token, playlist_id, tracks)
     Api::Client::place_first_track_to_the_end(access_token, playlist_id, tracks.length)
     # Api::Client::remove_last_track_from_playlist(access_token, playlist_id, tracks[tracks.length - 1])
-    @playlist_data = Api::Client::get_a_playlist_data(access_token, playlist_id)
+    Api::Client::get_a_playlist_data(access_token, playlist_id)
 end 
 
 @playlist_data = JSON.parse(create_playlist_for_my_user())
